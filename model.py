200671dc0a77b09f58e2ee5309f7f21d10ad0893
baconpancakess <baconpancakess@hotmail.com>
2024-04-13 08:16:59 -0300

    finished model (without testing)

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 #%%
+import torch
 from torch import nn
 import torch.nn.functional as F
 
@@ -35,12 +36,10 @@ class Unet(nn.Module):
         
         # Upward part
         for feature in reversed(features):
-            self.upward.append([
+            self.upward.append(
                 nn.ConvTranspose2d(in_channels = feature * 2, out_channel = feature, kernel_size = 2, stride = 2), 
-                Conv2D(in_channels = feature * 2, out_channels = feature)
-                ]
             )
-            self.in_channels = feature
+            self.upward.append(Conv2D(in_channels = feature * 2, out_channels = feature))
         
         # Bottleneck
         self.bottleneck = Conv2D(in_channels = feature[-1], out_channels = feature[-1] * 2)
@@ -49,9 +48,25 @@ class Unet(nn.Module):
         self.last_conv = nn.Conv2d(in_channels = in_channels, out_channels = out_channels, kernel_size = 1, stride = 1)
         
     def forward(self, x):
+        skip_connections = []
         
+        for down in self.downs:
+            x = down(x)
+            
+            skip_connections.append(x)
+            
+            x = self.pool(x)
         
+        x = self.bottleneck(x)
         
+        for i in range(len(self.upward), step = 2):
+            x = self.upward[i](x)
+            skip_connection_unit = skip_connections[-i//2]
+            concat = torch.cat([x, skip_connection_unit],dim=1)
+            x = self.ups[i + 1](concat)
         
+        x = self.last_conv(x)
         
-    
+        return x
+        
+# %%
