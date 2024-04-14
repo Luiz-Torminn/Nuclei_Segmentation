a14c2ce0ba2cbf58b7216bd385ef64098724f004
baconpancakess <baconpancakess@hotmail.com>
2024-04-14 09:41:19 -0300

    Fully functioning model

--- model.py
+++ model.py
@@ -2,11 +2,12 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+import torchvision.transforms.functional as TTF
 
 # %%
 class Conv2D(nn.Module):
     def __init__(self, in_channels:int, out_channels:int) -> None:
-        super(self, Conv2D).__init__()
+        super(Conv2D, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 1, bias = False),
             nn.BatchNorm2d(out_channels),
@@ -22,33 +23,34 @@ class Conv2D(nn.Module):
 # %%       
 class Unet(nn.Module):
     def __init__(self, in_channels:int = 1, out_channels:int = 1, features = [64,128,256,512]) -> None:
-        super(self, Unet).__init__()
+        super(Unet, self).__init__()
         self.in_channels = in_channels
         
-        self.down = nn.ModuleList()
+        self.downs = nn.ModuleList()
         self.upward = nn.ModuleList()
         self.pool = nn.MaxPool2d(2,2)
         
         # Downward part
         for feature in features:
-            self.down.append(Conv2D(in_channels = self.in_channels, out_channels = feature))
+            self.downs.append(Conv2D(in_channels = self.in_channels, out_channels = feature))
             self.in_channels = feature
         
         # Upward part
         for feature in reversed(features):
             self.upward.append(
-                nn.ConvTranspose2d(in_channels = feature * 2, out_channel = feature, kernel_size = 2, stride = 2), 
+                nn.ConvTranspose2d(in_channels = feature * 2, out_channels = feature, kernel_size = 2, stride = 2), 
             )
             self.upward.append(Conv2D(in_channels = feature * 2, out_channels = feature))
         
         # Bottleneck
-        self.bottleneck = Conv2D(in_channels = feature[-1], out_channels = feature[-1] * 2)
+        self.bottleneck = Conv2D(in_channels = features[-1], out_channels = features[-1] * 2)
         
         # Last Conv:
-        self.last_conv = nn.Conv2d(in_channels = in_channels, out_channels = out_channels, kernel_size = 1, stride = 1)
+        self.last_conv = nn.Conv2d(in_channels = features[0], out_channels = out_channels, kernel_size = 1, stride = 1)
         
     def forward(self, x):
         skip_connections = []
+        t_skip = reversed(skip_connections)
         
         for down in self.downs:
             x = down(x)
@@ -59,14 +61,30 @@ class Unet(nn.Module):
         
         x = self.bottleneck(x)
         
-        for i in range(len(self.upward), step = 2):
+        for i in range(0, len(self.upward), 2):
             x = self.upward[i](x)
-            skip_connection_unit = skip_connections[-i//2]
+            skip_connection_unit = list(reversed(skip_connections))[i//2]
+            
+            if x.shape != skip_connection_unit.shape:
+                x = TTF.resize(x, size=(skip_connection_unit.shape[2:]))
+                
             concat = torch.cat([x, skip_connection_unit],dim=1)
-            x = self.ups[i + 1](concat)
+            x = self.upward[i + 1](concat)
         
         x = self.last_conv(x)
         
         return x
         
+# %%
+# def test():
+#     rand_tensor = torch.rand((1,1,572,572))
+#     model = Unet(1,1)
+#     prediction = model(rand_tensor)
+#     print(rand_tensor.shape)
+#     print(prediction.shape)
+
+# %%
+# if __name__ == '__main__':
+#     test()
+
 # %%
